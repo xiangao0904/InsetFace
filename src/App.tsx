c5048bf8827b1907bf4eea79183d230579ee828e
TwilightFox <3150399699@qq.com>
2026-05-13 23:19:09 +0800

    update dl link

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Card, CardContent } from "@/components/ui/card";
 
 const REPO_URL = "https://github.com/xiangao0904/InsetFace";
 const INSTALLER_URL =
-  "https://github.com/xiangao0904/InsetFace/raw/master/installer/InsetFaceInstaller.exe";
+  "https://github.com/xiangao0904/InsetFace/releases/download/v1.0.0/InsetFaceInstaller.exe";
 const DEMO_VIDEO_URL = "/demo.mp4";
 const BLOG_URL = "https://xiangao0904.github.io/blog/insetface-algorithm";
 
